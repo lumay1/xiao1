1e0a017b97e8664832ae55846f5244b373fdb15e
lumay1 <lumayyingying@gmail.com>
2024-10-09 21:31:33 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -14,5 +13,8 @@ export default defineConfig({
   },
   esbuild: {
     charset: 'utf8',
+  },
+  build: {
+    outDir: 'docs',  // 设置输出目录为 'docs'
   },
 })
