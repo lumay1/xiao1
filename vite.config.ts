5ba6c93bd3e0856053d554cba5fcda5bf4aee277
lumay1 <lumayyingying@gmail.com>
2024-10-09 23:11:44 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/xiao1/', // 确保这里的base与你的GitHub仓库名称一致
+  base: '/xiao1/', // 确保这与您的仓库名称一致
 })
