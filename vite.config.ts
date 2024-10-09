ec4f1372aeb6a6e3e16a77b8cc9cc9cf3f887f42
lumay1 <lumayyingying@gmail.com>
2024-10-09 23:02:55 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+   import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/xiao1/',
-  build: {
-    outDir: '.',
-    emptyOutDir: false,
-  },
-})
+   export default defineConfig({
+     plugins: [react()],
+     base: '/xiao1/',
+     build: {
+       outDir: 'dist',
+     },
+   })
