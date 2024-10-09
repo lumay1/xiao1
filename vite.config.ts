025af922994b2646a55c129797574d7bea249fae
lumay1 <lumayyingying@gmail.com>
2024-10-09 23:09:17 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
-   import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
-   export default defineConfig({
-     plugins: [react()],
-     base: '/xiao1/',
-     build: {
-       outDir: 'dist',
-     },
-   })
+export default defineConfig({
+  plugins: [react()],
+  base: '/xiao1/', // 确保这里的base与你的GitHub仓库名称一致
+})
